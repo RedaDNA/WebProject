0a4b8bb397db6afa547eebaca4de3ff336cb82b3
Reda Shehab <reda.123.rrr@gmail.com>
2022-07-15 02:01:58 +0300

    model issue

--- DEMOPROJECT/DEMAPP/views.py
+++ DEMOPROJECT/DEMAPP/views.py
@@ -11,16 +11,19 @@ import joblib
 import tensorflow as tf
 def hi(request):
     return render(request ,'DEMAPP/hi.html')
-dir = os.path.dirname(__file__)
+dir = os.getcwd()
 
 def getPredicitions(userinput):
     #model = pickle.load(open('CNN-LSTMgradesdatasetsplited62%pre.sav', 'rb'))
     #model=joblib.load('CNN-LSTMgradesdatasetsplited62%pre.sav')
     #model=joblib.load('CBOW_grades_dataset_76_pre.pkl')
     #localhost_save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
-    model = tf.keras.models.load_model('CBOW_grades_dataset_76_pre')
+    print(dir)
+    #model = tf.keras.models.load_model('CBOW_grades_dataset_76_pre')
     #with gzip.open('Completed_model.joblib', 'rb') as f:
      #   model = pickle.load(f)
+    with open('CBOWgradesdataset76pre.sav', 'rb') as handle:
+        model = pickle.load(handle)
 
     userinput_scaled=standard_scalar(userinput,model)
     prediction = model.predict(userinput_scaled)
@@ -51,7 +54,7 @@ def home(request):
 def result(request):
     input=request.GET['userinput']
     #result = request.POST.get('userinput_scaled')
-    result=getPredicitionsDemo(input)
+    result=getPredicitions(input)
     print(result)
     #result = getPredicitions(userinput)
     #return render(request, 'result.html', {'result': result})
@@ -60,15 +63,11 @@ def result(request):
 
 
 def standard_scalar(userinputa,model):
-    userinput='When Kate and her parents visit the animal shelter, they select an adorable puppy, but they return to bring home an older dog they simply couldn\'t leave behind. Graham\'s humorous cartoon-style illustrations reveal hip, contemporary parents and a cozy domestic scene. This is an endearing story of family warmth and belonging'
-    tokenizer_input = Tokenizer(num_words=20000, char_level=False)
-    tokenizer_input.fit_on_texts(userinput)
-    sequences_input = tokenizer_input.texts_to_sequences(userinput)
-    word_index_input = tokenizer_input.word_index
-    print('Found %s unique tokens.' % len(word_index_input))
-    x_input =pad_sequences(sequences_input, maxlen=140)
-    print(x_input.shape)
-    for layer in model.layers:
-        print(layer.get_output_at(0).get_shape().as_list())
-    output_test = model.predict(x_input)
-    return  output_test
+    with open('tokenizer.pickle', 'rb') as handle:
+        loaded_tokenizer = pickle.load(handle)
+    seq = loaded_tokenizer.texts_to_sequences([userinputa])
+    padded = pad_sequences(seq, maxlen=140)
+    print(padded.shape)
+
+    pred = model.predict(padded)
+    return  pred
